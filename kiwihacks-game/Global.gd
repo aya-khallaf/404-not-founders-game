d2614f228aa89105acc3f222ae396979c3f66247
danielfay-ui <daniel_fay@paraparaumucollege.school.nz>
2026-09-26 12:15:36 +1200

    changes

---
+++ kiwihacks-game/Global.gd
@@ -0,0 +1,7 @@
+extends Node
+var player_node = null
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- kiwihacks-game/character_body_2d.gd
+++ kiwihacks-game/character_body_2d.gd
@@ -1,17 +1,22 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+
 var speed = 400
+@export var in_spaceship = false
 
 func _ready() -> void:
 	animated_sprite_2d.play("walk")
+	Global.player_node = self
 
 func _physics_process(delta):
+	if in_spaceship:
+		animated_sprite_2d.play("spaceship")
+		speed = 800
+	else:
+		animated_sprite_2d.play("walk")
+		speed = 400
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 
 	move_and_slide()
-
-
-func _on_animated_sprite_2d_animation_looped() -> void:
-	animated_sprite_2d.flip_h = !animated_sprite_2d.flip_h
