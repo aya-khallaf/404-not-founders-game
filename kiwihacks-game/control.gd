0a79e080f22bdde04167cecff9b4c7a14f690954
aya-khallaf <aya.es.khallaf@gmail.com>
2026-09-26 13:33:41 +1200

    system alert

---
+++ kiwihacks-game/control.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _ready() -> void:
+	$RichTextLabel2.visible_characters = 0
+	
+
+func _on_timer_timeout() -> void:
+	$RichTextLabel2.visible_characters+=1
+	if $RichTextLabel2.visible_characters == len($RichTextLabel2.text):
+		return
+	else:
+		$Timer.start()
