2e3e82284a670d3e90922918505b7bd402177077
danielfay-ui <daniel_fay@paraparaumucollege.school.nz>
2026-09-26 15:59:52 +1200

    A lot of stuff

--- kiwihacks-game/enemy.gd
+++ kiwihacks-game/enemy.gd
@@ -1,36 +1,50 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
-var toucing_player = false
+var touching_player = false
+var cooldown = false
+var health = 100
+var max_health = 100
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	$ProgressBar.value=health
+	$ProgressBar.max_value=max_health
+	if Global.player_node:
+		var direction = (Global.player_node.global_position - global_position).normalized()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if not touching_player:
+			velocity = direction * SPEED
+		else:
+			velocity = Vector2.ZERO
+			
+		if touching_player and not cooldown:
+			Global.player_node.health -= 5
+			cooldown = true
+			$Timer.start()
 
 	move_and_slide()
 
 
 func _on_hitbox_body_entered(body: Node2D) -> void:
-	if body.scene_file_path == "res://player.tscn":
-		toucing_player = true
+	if body == Global.player_node:
+		touching_player = true
 
 
 func _on_hitbox_body_exited(body: Node2D) -> void:
-	if body.scene_file_path == "res://player.tscn":
-		toucing_player = false
+	if body == Global.player_node:
+		touching_player = false
+
+
+func _on_timer_timeout() -> void:
+	cooldown = false
+
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	health-=10
+	if health<=0:
+		queue_free()
+
+
+func _on_hitbox_area_exited(area: Area2D) -> void:
+	pass # Replace with function body.
---
+++ kiwihacks-game/sword.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("attack"):
+		$Spritepaint27.visible = true
+		rotation+=1
+	else:
+		$Spritepaint27.visible = false
