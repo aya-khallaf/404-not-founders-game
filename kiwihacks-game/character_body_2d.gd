7cf2f3e3f62c59bbab78d69901ab86b650791655
wangzi2-web <wangzi2@wgc.school.nz>
2026-09-26 12:39:59 +1200

    Revert "Cyan"
    This reverts commit 217bf8f7a79f3bb9af5503431820ef8303962f39.

--- kiwihacks-game/character_body_2d.gd
+++ kiwihacks-game/character_body_2d.gd
@@ -1,7 +1,6 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-<<<<<<< Updated upstream
 
 var speed = 400
 @export var in_spaceship = false
@@ -9,12 +8,6 @@ var speed = 400
 func _ready() -> void:
 	animated_sprite_2d.play("walk")
 	Global.player_node = self
-=======
-var speed = 400
-
-func _ready() -> void:
-	animated_sprite_2d.play("walk")
->>>>>>> Stashed changes
 
 func _physics_process(delta):
 	if in_spaceship:
@@ -27,7 +20,3 @@ func _physics_process(delta):
 	velocity = direction * speed
 
 	move_and_slide()
-
-
-func _on_animated_sprite_2d_animation_looped() -> void:
-	animated_sprite_2d.flip_h = !animated_sprite_2d.flip_h
