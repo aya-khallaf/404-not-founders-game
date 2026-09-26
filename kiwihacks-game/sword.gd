93e56767d9cd71e3b70008afe8dbab17767eb123
danielfay-ui <daniel_fay@paraparaumucollege.school.nz>
2026-09-26 16:38:06 +1200

    stuff

--- kiwihacks-game/sword.gd
+++ kiwihacks-game/sword.gd
@@ -3,6 +3,6 @@ extends Area2D
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("attack"):
 		$Spritepaint27.visible = true
-		rotation+=1
+		rotation+=0.3
 	else:
 		$Spritepaint27.visible = false
