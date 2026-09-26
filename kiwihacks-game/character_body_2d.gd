217bf8f7a79f3bb9af5503431820ef8303962f39
wangzi2-web <wangzi2@wgc.school.nz>
2026-09-26 12:34:58 +1200

    Cyan

--- kiwihacks-game/character_body_2d.gd
+++ kiwihacks-game/character_body_2d.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+<<<<<<< Updated upstream
 
 var speed = 400
 @export var in_spaceship = false
@@ -8,6 +9,12 @@ var speed = 400
 func _ready() -> void:
 	animated_sprite_2d.play("walk")
 	Global.player_node = self
+=======
+var speed = 400
+
+func _ready() -> void:
+	animated_sprite_2d.play("walk")
+>>>>>>> Stashed changes
 
 func _physics_process(delta):
 	if in_spaceship:
@@ -20,3 +27,7 @@ func _physics_process(delta):
 	velocity = direction * speed
 
 	move_and_slide()
+
+
+func _on_animated_sprite_2d_animation_looped() -> void:
+	animated_sprite_2d.flip_h = !animated_sprite_2d.flip_h
